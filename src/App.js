bb31042e21c5b631bd838af70053ac9f1b381642
Lucas Pereira <lucaspereirasousa11@gmail.com>
2023-09-14 19:04:15 -0300

    test

--- src/App.js
+++ src/App.js
@@ -14,12 +14,3 @@ function App() {
 }
 
 export default App;
-
-
-Resumo para subir arquivos no github
-- git init (inicializa o repositprio do git vazio)
-- git branch -M main (troca o nome do branch principal para main)
-- git remote add origin + url (linka o repositorio do github com o local)
-obs: para verificar se o link deu certo o comando é git remote -v
-obs2: para ver o status dos arquivos o comando é git status 
-- git add . (adiciona todos os arquivos)
